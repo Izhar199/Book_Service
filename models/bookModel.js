10dbd0ff14436afc4e04caf26ec56801c615c332
Izhar199 <2023mt93356@wilp.bits-pilani.ac.in>
2024-11-22 17:49:38 +0530

    update

--- models/bookModel.js
+++ models/bookModel.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 
 const bookSchema = new mongoose.Schema({
+  bookId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
-  status: { type: String, enum: ["Available", "Checked Out"], default: "Available" },
-  publishedDate: { type: Date },
-  genre: { type: String },
+  availableCopies: { type: Number, required: true },
 });
 
 module.exports = mongoose.model("Book", bookSchema);
